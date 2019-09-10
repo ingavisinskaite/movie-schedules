a7cb3cdca96fe4964211522fffc24fb0a5cdb6a9
ingavisinskaite <ingavisinskaite@gmail.com>
2019-09-10 12:25:02 +0300

    added some styling to movie-card

--- src/components/movie-card/index.tsx
+++ src/components/movie-card/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import './style.less'
-import {Movie} from '../../models/movieDay';
-import {formatTime} from '../../services/calendarService';
+import { Movie } from '../../models/movieDay';
+import { formatTime } from '../../services/calendarService';
 
 interface IProps {
     info: Movie;
- }
+}
 
 const MovieCard = (props: IProps) => {
-        return (
-            <div className="movie-card">
-               <h2>{props.info.title}</h2>
-               <img className="movie-img" src={props.info.imgSrc}></img>
-               {
-                   props.info.cinemaSchedules.map((cinema, index) => {
-                       return <div key={index}><p>{cinema.title}: {cinema.times.map(time => formatTime(time))}</p></div>
-                   })
-               }
-            </div>
-        );
+    return (
+        <div className="movie-card">
+            <h2>{props.info.title}</h2>
+            <a href="https://www.imdb.com/">
+                <img className="movie-img" src={props.info.imgSrc}></img>
+            </a>
+            {
+                props.info.cinemaSchedules.map((cinema, index) => {
+                    return <div key={index}><p>{cinema.title}: {cinema.times.map(time => formatTime(time))}</p></div>
+                })
+            }
+
+        </div>
+    );
 }
 
 export default MovieCard;

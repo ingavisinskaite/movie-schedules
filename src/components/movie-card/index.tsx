c7e39619988dd029350dc841c1a9583765ddf717
ingavisinskaite <ingavisinskaite@gmail.com>
2019-09-16 13:55:00 +0300

    trying to add routing

--- src/components/movie-card/index.tsx
+++ src/components/movie-card/index.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import './style.less'
 import { Movie } from '../../models/movieDay';
 import { formatTime } from '../../services/calendarService';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import MoviePage from '../movie-page';
+
 
 interface IProps {
     info: Movie;
 }
 
 const MovieCard = (props: IProps) => {
-    return (
-        <div className="movie-card">
-            <h2>{props.info.title}</h2>
-            <a href="https://www.imdb.com/">
-                <img className="movie-img" src={props.info.imgSrc}></img>
-            </a>
-            {
-                props.info.cinemaSchedules.map((cinema, index) => {
-                    return <div key={index}><p>{cinema.title}: {cinema.times.map(time => formatTime(time))}</p></div>
-                })
-            }
 
-        </div>
+    return (
+        <Router>
+            <div className="movie-card">
+                <h2>{props.info.title}</h2>
+                <Link to="/movie">
+                    <img className="movie-img" src={props.info.imgSrc}></img>
+                </Link>
+                {
+                    props.info.cinemaSchedules.map((cinema, index) => {
+                        return <div key={index}><p>{cinema.title}: {cinema.times.map(time => formatTime(time))}</p></div>
+                    })
+                }
+
+
+                <Route exact path="/movie" component={MoviePage} />
+
+            </div>
+        </Router>
+
     );
 }
 
--- src/components/movie-page/index.tsx
+++ src/components/movie-page/index.tsx
@@ -8,7 +8,9 @@ interface IProps {
 const MoviePage = (props: IProps) => {
     return (
         <div>
-            <h1></h1>
+            <h1>This is a movie</h1>
         </div>
     )
 }
+
+export default MoviePage;
